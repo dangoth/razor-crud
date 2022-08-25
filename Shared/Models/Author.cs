0ad032b064fff56e16d1eb883ad516e7f2ec7dba
dangoth <alpha6667@gmail.com>
2022-08-25 22:06:45 +0200

    homework object relations

---
+++ Shared/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDB.Shared.Models;
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Biography Biography { get; set; }
+    public ICollection<Book> Books { get; set; }
+    public ICollection<CollaborationBook> CollaborationBooks { get; set; }
+}
